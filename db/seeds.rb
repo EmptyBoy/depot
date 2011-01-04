096da5bfbc48234efa225c789b36959479243465
EmptyBoy <sobaches@mail.ru>
2011-01-04 19:02:10 +0300

    Task A: Scaffolding

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,11 @@
 #
 #   cities = City.create([{ :name => 'Chicago' }, { :name => 'Copenhagen' }])
 #   Mayor.create(:name => 'Daley', :city => cities.first)
+
+Product.delete_all
+#
+Product.create( :title => 'Test product',
+                :description => "Some text.\n Blabla.",
+                :image_url   => '/images/ruby.jpg',
+                :price       => 39.90 )
+#
