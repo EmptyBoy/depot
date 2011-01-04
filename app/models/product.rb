bd62429354b32215055f56000588970088ce126c
EmptyBoy <sobaches@mail.ru>
2011-01-04 22:41:26 +0300

    Task C: Catalog display.

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,4 +1,7 @@
 class Product < ActiveRecord::Base
+  # order by title
+  default_scope :order => 'title'
+  # validations
   validates :title, :description, :image_url, :presence => true
   validates :price, :numericality => { :greater_than_or_equal_to => 0.01 }
   validates :title, :uniqueness => true
